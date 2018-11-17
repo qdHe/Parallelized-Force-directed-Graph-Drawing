2094ddb8e8b1cfce19cfaeb06f9db4dacc2c0c5a
qdHe <qiduhe@outlook.com>
2018-11-17 18:22:47 -0500

    seq 0.82 1d edge

--- main.cpp
+++ main.cpp
@@ -2,12 +2,14 @@
 #include <fstream>
 #include <algorithm>
 #include <stdlib.h>
-#include <cmath>
+#include <math.h>
 using namespace std;
 
-#define K 0.1
 #define W 10
 #define H 10
+int N = 25;//tentative
+int M = 35;
+int K = sqrt(W*H/N);
 
 class Vertex{
 public:
@@ -16,8 +18,23 @@ public:
     float disp_x;
     float disp_y;
 
-    Vertex():x(float(rand())/RAND_MAX), y(float(rand())/RAND_MAX), disp_x(0), disp_y(0){}
+    Vertex():x((float(rand())/RAND_MAX-0.5)), y((float(rand())/RAND_MAX-0.5)), disp_x(0), disp_y(0){}
 };
+
+class Edge{
+public:
+    int idx1;
+    int idx2;
+
+    Edge():idx1(-1), idx2(-1){}
+    Edge(int a, int b):idx1(a), idx2(b){}
+};
+
+bool cmp(Edge a, Edge b) {
+    if (a.idx1<b.idx1) return true;
+    else if (a.idx1 == b.idx1 && a.idx2 < b.idx2) return true;
+    return false;
+}
 
 float max(float a, float b){
     return (a>b)? a:b;
@@ -26,64 +43,105 @@ float min(float a, float b){
     return (a<b)? a:b;
 }
 inline float repulsive_force(float dist){
-    return K*K/dist/10000;
+    return K*K/dist/N/10000;
 }
 
 inline float attractive_force(float dist){
-    return dist*dist/K/10000;
+    return dist*dist/K/N;
 }
 
-void force_directed(Vertex *V, int **E, int N, int Iteration, float thr){
+void force_directed(Vertex *V, Edge *E, int *Idx, int N, int Iteration, float thr){
     for(int itr=0; itr<Iteration; ++itr){
-        cout << "Iteration = " << itr+1 << endl;
+        //if (itr%(Iteration/10) == 0) cout << "Iteration = " << itr+1 << endl;
         for(int v=0; v<N; ++v){
             for(int u=v+1; u<N; ++u){
                 float d_x = V[v].x-V[u].x;
                 float d_y = V[v].y-V[u].y;
                 float dist = sqrt(d_x*d_x+d_y*d_y);
-                dist = max(dist, 0.0001);
+                dist = max(dist, 0.001);
                 float rf = repulsive_force(dist);
-                float af = E[v][u]*attractive_force(dist);
                 //if(v%1000 && u%1000) cout<<rf<<' '<<af<<' '<<dist<<endl;
-                V[v].disp_x += d_x/dist*(rf-af);
-                V[v].disp_y += d_y/dist*(rf-af);
-                V[u].disp_x += d_x/dist*(af-rf);
-                V[u].disp_y += d_y/dist*(af-rf);
+                V[v].disp_x += d_x/dist*rf;
+                V[v].disp_y += d_y/dist*rf;
+                V[u].disp_x -= d_x/dist*rf;
+                V[u].disp_y -= d_y/dist*rf;
+            }
+            if (v == 0){
+                for(int e=0; e<Idx[v]; ++e){
+                    int u = E[e].idx2;
+                    float d_x = V[v].x-V[u].x;
+                    float d_y = V[v].y-V[u].y;
+                    float dist = sqrt(d_x*d_x+d_y*d_y);
+                    dist = max(dist, 0.001);
+                    float af = attractive_force(dist);
+                    V[v].disp_x -= d_x/dist*af;
+                    V[v].disp_y -= d_y/dist*af;
+                }
+            }
+            else {
+                for(int e=Idx[v-1]; e<Idx[v]; ++e){
+                    int u = E[e].idx2;
+                    float d_x = V[v].x-V[u].x;
+                    float d_y = V[v].y-V[u].y;
+                    float dist = sqrt(d_x*d_x+d_y*d_y);
+                    dist = max(dist, 0.001);
+                    float af = attractive_force(dist);
+                    V[v].disp_x -= d_x/dist*af;
+                    V[v].disp_y -= d_y/dist*af;
+                }
             }
         }
+
         for(int v=0; v<N; ++v){
             //if(v%1000==0) cout<<V[v].disp_x<<' '<<V[v].disp_y<<endl;
             float dist = sqrt(V[v].disp_x*V[v].disp_x + V[v].disp_y*V[v].disp_y);
             V[v].x += (dist > thr)? V[v].disp_x/dist*thr : V[v].disp_x;
             V[v].y += (dist > thr)? V[v].disp_y/dist*thr : V[v].disp_y;
-            V[v].x = min(1, max(0,V[v].x));
-            V[v].y = min(1, max(0,V[v].y));
+            V[v].x = min(W/2, max(-W/2,V[v].x));
+            V[v].y = min(H/2, max(-H/2,V[v].y));
             V[v].disp_x = 0;
             V[v].disp_y = 0;
         }
-        thr -= 0.01;
+        thr *= 0.99;
     }
 }
 
-int main() {
-    int N = 9000;//tentative
+int main(int argc, char* argv[]) {
     Vertex *V = new Vertex[N];
-    int **E = new int*[N];
-    for (int i=0; i<N; ++i){
-        E[i] = new int[N]();
-    }
+    Edge *E = new Edge[2*M];
+    int *Idx = new int[N]();
+
     ifstream infile;
-    infile.open("data/Wiki-Vote.txt");
-    int idx1, idx2;
+	cout<<argv[0]<<endl;
+    infile.open(argv[1]);
+    //int idx1, idx2, w;
+    int ct = 0;
+    Edge e;
     while(!infile.eof()){
-        infile >> idx1 >> idx2;
+        infile >> e.idx1 >> e.idx2;
         //cout<<idx1<<' '<<idx2<<endl;
-        E[idx1][idx2] = E[idx2][idx1] = 1;
+        E[ct++] = e;
+        swap(e.idx1, e.idx2);
+        E[ct++] = e;
     }
-    cout << "Complete Initialization" << endl;
-    int iteration = 10;
-    int thr = 1;
-    force_directed(V, E, N, iteration, thr);
+    sort(E, E+ct, cmp);
+    cout << "Total Edges = " << ct/2 << endl;
+	//for(int i=0; i<20; ++i){
+	//	cout<<E[i].idx1<<' '<<E[i].idx2<<endl;
+	//}
+    for(int i=0; i<ct; ++i) {
+        Idx[E[i].idx1] += 1;
+    }
+	//cout<<"IDX:[";
+    for(int i=1; i<N; ++i) {
+        Idx[i] += Idx[i-1]; //End Index
+		//cout<<Idx[i]<<' ';
+    }
+	//cout<<"]"<<endl;
+    //cout << "Complete Initialization" << endl;
+    int iteration = atoi(argv[2]);
+    int thr = W+H;
+    force_directed(V, E, Idx, N, iteration, thr);
     ofstream outfile("Vertex_Pos.txt");
     for (int v=0; v<N; ++v){
         outfile << V[v].x <<' '<<V[v].y<<endl;
--- plot.py
+++ plot.py
@@ -5,17 +5,23 @@ edge_fn = sys.argv[2]
 f = open(fn, 'r')
 f1 = plt.figure(1)
 cords = []
+cnt = 0
 for line in f.readlines():
     cord = line.split()
     cords.append([(float)(cord[0]), (float)(cord[1])])
     plt.scatter((float)(cord[0]), (float)(cord[1]), 1,color='r')
+    #cnt += 1
+    #if cnt > 1000:
+    #    break
 f.close()
 f = open(edge_fn, 'r')
 for line in f.readlines():
     cord = line.split()
-    print(cord)
     in1 = (int)(cord[0])
     in2 = (int)(cord[1])
-    plt.plot([cords[in1][0],cords[in2][0]], [cords[in1][1],cords[in2][1]], linewidth=1, color='b')
+    #if in1 >= 1000 or in2 >= 1000:
+    #    continue
+    plt.plot([cords[in1][0],cords[in2][0]], [cords[in1][1],cords[in2][1]],
+				linewidth=1, color='b', alpha=0.1)
 f.close()
 plt.show()
